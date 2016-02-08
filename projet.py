f7ce5de9ca1aaa5bd7bd5e8ac53a607395665ff8
BECIRSPAHIC LUCAS <3407027@ppti-14-308-09.ufr-info-p6.jussieu.fr>
2016-02-08 19:53:27 +0100

    update

--- projet.py
+++ projet.py
@@ -17,7 +17,7 @@ class FonceurStrategy(BaseStrategy):
         BaseStrategy.__init__(self,"FonceurStrategy")
     def compute_strategy(self, state, teamid,player):
         etat = PlayerDecorator(state, teamid, player)
-        return etat.go_ball + etat.shoot_but
+        return etat.go_ball + etat.gere_shoot
         
 class GardienStrategy(BaseStrategy):
     def __init__(self):
@@ -28,7 +28,7 @@ class GardienStrategy(BaseStrategy):
             return etat.go(etat.my_but)
         else:
             if (etat.my_position.distance(etat.ball_position) < 30):
-                return etat.go_ball + etat.shoot_but
+                return etat.go_ball + etat.gere_shoot
             else:
                 return etat.go(etat.my_but)
 
@@ -58,14 +58,33 @@ class DefenseurStrategy(BaseStrategy):
          return defenseur(etat)
          
 def defenseur(etat):
-    if etat.adv_in_my_zone:
-        if (etat.adv_proche_distance < 30):
-            return etat.go_ball + etat.shoot(etat.equ_proche)
+    if (etat.distance_ball < 20):
+        V = etat.go_ball
+    else:
+        V = etat.go(etat.my_but)
+    return V + etat.gere_shoot
+
+class ZoneStrategy(BaseStrategy):
+    def __init__(self):
+        BaseStrategy.__init__(self,"ZoneStrategy")
+    def compute_strategy(self, state, teamid,player):
+        etat = PlayerDecorator(state,teamid,player)
+        if (etat.ball_in_my_zone):
+            if etat.distance_ball < 20:
+                V = etat.go_ball
+            else:
+                V =  etat.go(etat.my_but)
         else:
-            return etat.go(etat.my_but)
-    if etat.can_shoot:
-        return etat.go_ball + etat.shoot_but
-    return etat.go(milieu(etat.my_zone))
+            V =  etat.go(etat.adv_but)
+        
+        if etat.can_shoot:
+            if (etat.equ_in_my_zone):
+                return V + etat.passe
+            else:
+                return V + etat.shoot_but
+        else:
+            return V
+
 
 Defens = StratStateless(defenseur)
 
@@ -73,10 +92,11 @@ joueur1 = Player("Joueur 1", FonceurStrategy())
 joueur2 = Player("Joueur 2", GardienStrategy())
 joueur3 = Player("Joueur 3", PasseurStrategy())
 joueur4 = Player("Joueur 4", DefenseurStrategy())
+joueur5 = Player("Joueur 5", ZoneStrategy())
 
-#team1 = SoccerTeam("team1",[joueur3,joueur2])
-#team2 = SoccerTeam("team2",[joueur4,joueur2])
-team1 = SoccerTeam("team1",[joueur1,joueur2,joueur3,joueur3])
-team2 = SoccerTeam("team2",[joueur1,joueur2,joueur3,joueur3])
+#team1 = SoccerTeam("team1",[joueur1])
+#team2 = SoccerTeam("team2",[joueur5])
+team1 = SoccerTeam("team1",[joueur1,joueur2,joueur5,joueur5])
+team2 = SoccerTeam("team2",[joueur1,joueur2,joueur3,joueur5])
 match = SoccerMatch(team1, team2)
-#soccersimulator.show(match)
+soccersimulator.show(match)
--- zone.py
+++ zone.py
@@ -8,14 +8,32 @@ class zone(object):
     def __init__(self,bg,hd):
         self.bg = bg
         self.hd = hd
+        self.n_bg = Vector2D(-1,-1)
+        self.n_hd = Vector2D(1,1)
 
-def est_dans(self,pos):
-    if (pos.x < self.bg.x or pos.x > self.hd.x):
-        return False
-    if (pos.y < self.bg.y or pos.y > self.hd.y):
-        return False
-    return True
+    def normalisation(self,Vecteur):
+        Vecteur.x = (Vecteur.x * 2 / self.hd.x - self.bg.x) - 1
+        Vecteur.y = (Vecteur.y * 2 / self.hd.y - self.bg.y) - 1
+        return Vecteur
+    
+    def denormalisation(self, Vecteur):
+        Vecteur.x = (Vecteur.x + 1)* (self.hd.x - self.bg.x) / 2
+        Vecteur.y = (Vecteur.y + 1)* (self.hd.y - self.bg.y) / 2
+        return Vecteur
+    
+    def est_dans(self,pos):
+        if (pos.x < self.bg.x or pos.x > self.hd.x):
+            return False
+        if (pos.y < self.bg.y or pos.y > self.hd.y):
+            return False
+        return True
 
-def milieu(self):
-    return Vector2D(self.bg.x + self.hd.x / 2, self.bg.y + self.hd.y / 2)
+    def mirroir(self,Vecteur):
+        V = self.normalisation(Vecteur)
+        V.x = -(V.x)
+        V = self.denormalisation(Vecteur)
+        return V
 
+    
+
+terrain = zone(Vector2D(0,0),Vector2D(settings.GAME_WIDTH, settings.GAME_HEIGHT))
