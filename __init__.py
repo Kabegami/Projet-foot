eeab0756afd3f571756f43d3075db986e92736da
Lucas <lucas.becirspahic@gmail.com>
2016-02-14 00:00:57 +0100

    p

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
 from projet import *
 
-joueur1 = Player("Joueur1",FonceurStrategy())
-joueur2 = Player("Joueur2",GardienStrategy())
-joueur3 = Player("Joueur3",DefenseurStrategy())
-joueur4 = Player("Joueur4",PasseurStrategy())
-joueur5 = Player("Joueur5",ZoneStrategy())
+joueur1 = Player("Joueur 1", FonceurStrategy())
+joueur2 = Player("Joueur 2", GoalStrategy())
+joueur3 = Player("Joueur 3", MilieuStrategy())
 
 team1 = SoccerTeam("team1",[joueur1])
-team2 = SoccerTeam("team2",[joueur1,joueur5])
-team4 = SoccerTeam("team4",[joueur1,joueur4,joueur5,joueur5])
+team2 = SoccerTeam("team2",[joueur1,joueur2])
+team4 = SoccerTeam("team4",[joueur1,joueur2,joueur1,joueur1])
